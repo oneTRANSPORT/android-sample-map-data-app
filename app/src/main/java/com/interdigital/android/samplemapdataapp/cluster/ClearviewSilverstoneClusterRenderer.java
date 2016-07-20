0d5ba54230baecd2f97e572eec2d8d689d2edba0
PaulThomas <dr.paul.thomas@gmail.com>
2016-07-20 15:46:32 +0100

    Added description pop-up window for Silverstone car parks.

--- app/src/main/java/com/interdigital/android/samplemapdataapp/cluster/ClearviewSilverstoneClusterRenderer.java
+++ app/src/main/java/com/interdigital/android/samplemapdataapp/cluster/ClearviewSilverstoneClusterRenderer.java
@@ -3,6 +3,7 @@ package com.interdigital.android.samplemapdataapp.cluster;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
@@ -29,52 +30,18 @@ public class ClearviewSilverstoneClusterRenderer
 
     @Override
     public View getInfoContents(Marker marker) {
-//        ClearviewSilverstoneClusterItem clearviewSilverstoneClusterItem = getClusterItem(marker);
-//        String comment = clearviewSilverstoneClusterItem.getComment();
-//        if (!comment.contains(": Event Location :") && !comment.contains(": Location :")) {
-//             Unformatted text, very difficult to do anything else here.
-//            TextView textView = new TextView(context);
-//            textView.setTextColor(0xff000000);
-//            textView.setText(comment);
-//            return textView;
-//        } else {
-            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(
-                    Context.LAYOUT_INFLATER_SERVICE);
-            View view = layoutInflater.inflate(R.layout.pop_up_road_works, null, false);
-//            TextView reasonTextView = (TextView) view.findViewById(R.id.reason_text_view);
-//            TextView closuresTextView = (TextView) view.findViewById(R.id.closures_text_view);
-//            TextView locationTextView = (TextView) view.findViewById(R.id.location_text_view);
-//            String reason = "";
-//            String location = "";
-//            String closures = "";
-//            if (comment.contains(": Event Location :")) {
-//                reason = comment.replaceFirst("^.*: Event Reason :", "").replaceFirst(":.*$", "").trim();
-//                location = comment.replaceFirst("^.*: Event Location :", "").replaceFirst(":.*$", "").trim();
-//                if (location.isEmpty()) {
-//                    locationTextView.setVisibility(View.GONE);
-//                }
-//                closures = comment.replaceFirst("^.*: Lane Closures :", "").replaceFirst(":.*$", "").trim();
-//                if (closures.startsWith("TYPE") || closures.trim().isEmpty()) {
-//                    closuresTextView.setVisibility(View.GONE);
-//                }
-//            }
-//            if (comment.contains(": Location :")) {
-//                reason = comment.replaceFirst("^.*: Reason :", "").replaceFirst(":.*$", "").trim();
-//                location = comment.replaceFirst("^.*: Location :", "").replaceFirst(":.*$", "").trim();
-//                if (location.isEmpty()) {
-//                    locationTextView.setVisibility(View.GONE);
-//                }
-//                closures = comment.replaceFirst("^.*: Lane Closures :", "").replaceFirst(":.*$", "").trim();
-//                if (closures.startsWith("TYPE") || closures.trim().isEmpty()) {
-//                    closuresTextView.setVisibility(View.GONE);
-//                }
-//            }
-//            location = location.replaceFirst("^The ", "");
-//            reasonTextView.setText(reason);
-//            closuresTextView.setText(closures);
-//            locationTextView.setText(location);
-            return view;
-//        }
+        ClearviewSilverstoneClusterItem clearviewSilverstoneClusterItem = getClusterItem(marker);
+        String description = clearviewSilverstoneClusterItem.getDescription();
+        String changed = "Device installation: " + clearviewSilverstoneClusterItem.getChanged().trim()
+                .replaceFirst(" .*$", "");
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.pop_up_silverstone_car_park, null, false);
+        TextView signTextView = (TextView) view.findViewById(R.id.sign_text_view);
+        TextView changedTextView = (TextView) view.findViewById(R.id.changed_text_view);
+        signTextView.setText(description);
+        changedTextView.setText(changed);
+        return view;
     }
 
 }
